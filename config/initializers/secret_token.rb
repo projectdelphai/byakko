b07fe34028eedeee71897ac7ce6993176bbe2d4e
Reuben Castelino <projectdelphai@gmail.com>
2013-04-10 22:06:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Byakko::Application.config.secret_token = '0db3dc241c64f23274e93389906183fda953fb0f4c57456b19c1d80ab82cebbc2885d1b5857f6af6c2e4d153a29da01caead2b9fa0c209859d78a8f7a730a9dd'
