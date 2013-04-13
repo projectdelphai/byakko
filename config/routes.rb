e5d98da8914ed1c123ea23b64d5ab95354f23cb9
Reuben Castelino <projectdelphai@gmail.com>
2013-04-12 21:20:21 -0700

    added signin and user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Byakko::Application.routes.draw do
   get "home/index"
 
+  resources :users
+
+  root to: 'home#index'
+
+  match '/signup', to: 'users#new'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -50,7 +56,6 @@ Byakko::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
