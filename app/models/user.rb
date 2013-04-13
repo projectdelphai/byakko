74ad8154e02d6059b58ba9bc685709843019e513
Reuben Castelino <projectdelphai@gmail.com>
2013-04-12 19:48:35 -0700

    password model and basics

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :username
+  has_secure_password
+  
+  attr_accessible :username, :password, :password_confirmation
+
+  validates :password, presence: true, length: { within: 5..30 }
+  validates :password_confirmation, presence: true
+  validates :username, presence: true, length: { within: 5..30 }, uniqueness: true
 end
