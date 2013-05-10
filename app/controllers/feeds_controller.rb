5fdee02250af76b8f26214a2063e3335effca254
Reuben Castelino <projectdelphai@gmail.com>
2013-05-09 22:33:14 -0700

    fixed small bug in creating rss feed

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -7,7 +7,10 @@ class FeedsController < ApplicationController
   end
 
   def newchapters
-    response = JSON.load(HTTParty.get("http://localhost:8080/users/2.json?api_key=#{params['api_key']}").body)
+    current_user = User.find_by_api_key(params['api_key'])
+    @current_user = current_user.username
+    current_user_id = current_user_id
+    response = JSON.load(HTTParty.get("http://byakko.heroku.com/users/#{current_user_id}.json?api_key=#{params['api_key']}").body)
     @newchapters = response['manga']
     @current_user = User.find_by_api_key(params['api_key']).username
   end
