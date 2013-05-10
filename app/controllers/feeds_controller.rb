750ba2add1102e83af67ca65c3c87185fbf81d84
Reuben Castelino <projectdelphai@gmail.com>
2013-05-09 22:37:45 -0700

    typo

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -9,7 +9,7 @@ class FeedsController < ApplicationController
   def newchapters
     current_user = User.find_by_api_key(params['api_key'])
     @current_user = current_user.username
-    current_user_id = current_user_id
+    current_user_id = current_user.id
     response = JSON.load(HTTParty.get("http://byakko.heroku.com/users/#{current_user_id}.json?api_key=#{params['api_key']}").body)
     @newchapters = response['manga']
     @current_user = User.find_by_api_key(params['api_key']).username
