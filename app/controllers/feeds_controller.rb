132940a7e90e9e380374d02cbb6a82038921ce52
Reuben Castelino <projectdelphai@gmail.com>
2013-06-04 23:01:59 -0700

    fixed rss feed

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -8,9 +8,9 @@ class FeedsController < ApplicationController
   end
 
   def newchapters
-    current_user = User.find_by_api_key(params['api_key'])
-    response = JSON.load(HTTParty.get("http://byakko.heroku.com/users/#{current_user.id}.json?api_key=#{params['api_key']}").body)
-    #response = JSON.load(HTTParty.get("http://localhost:8080/users/#{current_user.id}.json?api_key=#{params['api_key']}").body)
+    @user = User.find_by_api_key(params['api_key'])
+    response = JSON.load(HTTParty.get("http://byakko.heroku.com/users/#{@user.id}.json?api_key=#{params['api_key']}").body)
+    #response = JSON.load(HTTParty.get("http://localhost:8080/users/#{@user.id}.json?api_key=#{params['api_key']}").body)
     response['manga'].delete_if { |x| x['newchapter'].to_i <= 0 }
     @new_manga = response['manga']
     @new_manga.each { |manga|
--- app/views/feeds/newchapters.rss.builder
+++ app/views/feeds/newchapters.rss.builder
@@ -2,8 +2,8 @@ xml.instruct!
 xml.rss :version => '2.0', 'xmlns:atom' => 'http://www.w3.org/2005/Atom' do
 
   xml.channel do
-    xml.title "New Chapters for #{current_user.username}"
-    xml.description "RSS feed of new chapters for #{current_user.username}'s subscribed manga"
+    xml.title "New Chapters for #{@user.username}"
+    xml.description "RSS feed of new chapters for #{@user.username}'s subscribed manga"
     xml.link root_url
     xml.language 'en'
     xml.tag! 'atom:link', :rel => 'self', :type => 'application/rss+xml', :href => feed_url
